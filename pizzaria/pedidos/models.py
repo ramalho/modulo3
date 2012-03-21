9ff58557ebe26e4d779dd2f3eae33176e90a7153
Luciano Ramalho <luciano@ramalho.org>
2012-03-20 23:21:29 -0300

    aplicacao pedidos comecada

---
+++ pizzaria/pedidos/models.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+from django.db import models
+
+# Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=128)
+    fone = models.CharField(max_length=16)
+    email = models.EmailField(blank=True)
+    
+    def __unicode__(self):
+        return self.nome
+
+# XXX: colocar no admin.py
+from django.contrib import admin
+admin.site.register(Cliente)
+
