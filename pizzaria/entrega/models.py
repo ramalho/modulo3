ee8fdc52ff14b0f55eb2eadae131ef4042394763
Luciano Ramalho <luciano@ramalho.org>
2012-03-22 23:12:37 -0300

    novos campos para o models

--- pizzaria/entrega/models.py
+++ pizzaria/entrega/models.py
@@ -5,9 +5,17 @@ from django.db import models
 # Create your models here.
 
 class Cliente(models.Model):
-    nome = models.CharField(max_length=128)
-    fone = models.CharField(max_length=16)
+    nome = models.CharField(max_length=128, db_index=True)
+    fone = models.CharField(max_length=16, db_index=True)
+    ramal = models.CharField(max_length=4, blank=True)
     email = models.EmailField(blank=True)
+    logradouro = models.CharField(max_length=32, db_index=True)
+    numero = models.PositiveIntegerField(u'número')
+    complemento = models.CharField(max_length=32, blank=True)
+    obs = models.TextField(u'observação')
+    
+    class Meta:
+        unique_together = ('fone', 'ramal')
         
     def __unicode__(self):
         return self.nome
