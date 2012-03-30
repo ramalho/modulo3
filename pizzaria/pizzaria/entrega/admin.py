29631e02b1e5adcac2d2119720262a2c3d38247b
Luciano Ramalho <luciano@ramalho.org>
2012-03-29 22:55:48 -0300

    inclusao do modelo de pizza e mais configuracoes da interface administrativa do Django

--- pizzaria/pizzaria/entrega/admin.py
+++ pizzaria/pizzaria/entrega/admin.py
@@ -1,8 +1,10 @@
 # coding: utf-8
 
 from django.contrib import admin
+from django.forms import Textarea
+from django.db.models import TextField
 
-from .models import Cliente, Pedido, Entregador
+from .models import Cliente, Pedido, Entregador, Pizza
 
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ('fone', 'nome', 'endereco')
@@ -10,8 +12,16 @@ class ClienteAdmin(admin.ModelAdmin):
     search_fields = ['fone', 'nome', 'logradouro', 'numero']
     list_filter = ('logradouro',)
 
+class PizzaAdmin(admin.TabularInline):
+    model = Pizza
+    formfield_overrides = {
+        TextField: {'widget': Textarea(attrs={'rows':2, 'cols':10})},
+    }
+
 class PedidoAdmin(admin.ModelAdmin):
     list_display = ('hora_inclusao', 'cliente', 'pronto', 'partiu')
+    list_select_related = True
+    date_hierarchy = 'inclusao'
     
     def hora_inclusao(self, obj):
         return obj.inclusao.strftime('%H:%M')
@@ -20,7 +30,10 @@ class PedidoAdmin(admin.ModelAdmin):
         return bool(obj.pronto and obj.entregador and obj.partida)
     partiu.boolean = True
     
+    inlines = [PizzaAdmin]
+    
     
 admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Pedido, PedidoAdmin)
 admin.site.register(Entregador)
+
--- pizzaria/pizzaria/entrega/models.py
+++ pizzaria/pizzaria/entrega/models.py
@@ -16,9 +16,13 @@ class Cliente(models.Model):
     
     class Meta:
         unique_together = ('fone', 'ramal')
+        ordering = ['fone', 'ramal']
         
     def __unicode__(self):
-        return self.nome
+        fone = self.fone
+        if self.ramal:
+            fone += ' r.' + self.ramal
+        return fone + '-' + self.nome
         
     def endereco(self):
         return u'%s, %s' % (self.logradouro, self.numero)
@@ -30,6 +34,9 @@ class Pedido(models.Model):
     pronto = models.BooleanField(default=False)
     entregador = models.ForeignKey('Entregador', null=True, blank=True)
     partida = models.TimeField(null=True, blank=True)
+    
+    def __unicode__(self):
+        return self.inclusao.strftime('%H:%M') + ' - ' + self.cliente.nome
 
 class Entregador(models.Model):
     nome = models.CharField(max_length=64)
@@ -39,18 +46,35 @@ class Entregador(models.Model):
         
     class Meta:
         verbose_name_plural = u'Entregadores'
+
+SABORES = [
+    ('mussarela', 'Mussarela'),
+    ('portuguesa', 'Portuguesa'),
+    ('calabresa', 'Calabresa'),
+    ('atum', 'Atum'),
+]
     
+class Pizza(models.Model):
+    pedido = models.ForeignKey(Pedido)
+    sabor1 = models.CharField(u'sabor 1', max_length=32, choices=SABORES)
+    coberto1 = models.BooleanField(u'cob.')
+    sabor2 = models.CharField(u'sabor 2', max_length=32, choices=SABORES, blank=True)
+    coberto2 = models.BooleanField(u'cob.')
+    obs = models.TextField(u'observação', blank=True) 
+
+    def __unicode__(self):
+        sabor = self.sabor1
+        if self.coberto1:
+            sabor += ' coberta'
+        if self.sabor2:
+            sabor2 = self.sabor2    
+            if self.coberto2:
+                sabor2 += ' coberta'
+            sabor = u'½ %s, ½ %s' % (sabor, sabor2)
+        return sabor
+            
 
 
 
 
-
-
-
-
-
-
-
-		
-		
 
